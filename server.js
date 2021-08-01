f3642163425b0590a1264c9d0963a4e83e893330
squad15 <squad15@iconit.com.br>
2021-08-01 15:15:52 -0300

    Implementado funcionalidade de envio de mensagens e recuperacao em memoria

--- server.js
+++ server.js
@@ -14,4 +14,16 @@ app.use('/', (req, res) => {
   res.render('index.html');
 });
 
+let messages = [];
+
+io.on('connection', socket => {
+
+  socket.emit('previousMessages', messages);
+
+  socket.on('sendMessage', data => {
+    messages.push(data);
+    socket.broadcast.emit('receiveMessage', data);
+  });
+});
+
 server.listen(3000);
